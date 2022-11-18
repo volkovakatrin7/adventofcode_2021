a605b8325513b411df69bbb0a61d5d8409b9f514
volkovakatrin <77300163+volkovakatrin@users.noreply.github.com>
2022-11-18 14:43:24 +0100

    works

--- 6.py
+++ 6.py
@@ -2,25 +2,54 @@ def split_comma(text):
     return text.split(",")
 
 def main():
-    with open("data6a.txt", "r") as f:
+    with open("data6.txt", "r") as f:
         reader = f.read()
 
         data = split_comma(reader)
         fish = list(map(int, data))
-        print(fish)
+        #print(fish)
 
-        for i in range(30):
-            print("After day", i+1, end=" ")
+        """
+        for i in range(21):
+            print("After day", i+1, end=" - ")
             for i in range(len(fish)):
                 if fish[i] == 0:
                     fish[i] = 7
-                    fish.append(-8)
-                elif fish[i] == -9:
+                    fish.append(-1)
+                elif fish[i] == -2:
                     fish[i] = 7
                 fish[i] -= 1
-            print(fish)
+            print(len(fish))
 
         print(len(fish))
+        """
+        population = [0,0,0,0,0,0,0,0,0]
+        for i in range(9):
+            for f in fish:
+                if f == i:
+                    population[i] +=1
+
+        days = 256
+        total = [population]
+
+        for day in range(days):
+            new = [0,0,0,0,0,0,0,0,0]
+            new[0] = total[day][1]
+            new[1] = total[day][2]
+            new[2] = total[day][3]
+            new[3] = total[day][4]
+            new[4] = total[day][5]
+            new[5] = total[day][6]
+            new[6] = total[day][0] + total[day][7]
+            new[7] = total[day][8]
+            new[8] = total[day][0]
+            total.append(new)
+
+        total_of_fish = 0
+        for item in total[days]:
+            total_of_fish += item
+        print(total_of_fish)
+
 
 if __name__ == "__main__":
     main()
---
+++ 7.py
@@ -0,0 +1,28 @@
+def main():
+    with open("data7.txt", "r") as f:
+        reader = f.read()
+
+    crabs = [int(x) for x in reader.split(",")]
+    #print(crabs)
+
+    last = max(crabs)
+
+    total_fuel = []
+    for position in range(0, last+1):
+        fuel = 0
+        for crab in crabs:
+            moves = abs(position - crab)
+            f = moves*(moves+1)/2
+            #print(f)
+            fuel += f
+        #print("Total ", fuel)
+        total_fuel.append(fuel)
+
+    #print(total_fuel)
+    optimum = min(total_fuel)
+    print(optimum)
+
+
+
+if __name__ == "__main__":
+    main()
